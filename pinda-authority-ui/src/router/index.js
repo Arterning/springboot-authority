4afb612509ec47fb8126af8a279e41439e2f9bc4
Arterning <ning.huang.ridson@gmail.com>
2023-02-18 15:59:15 +0800

    解决前端动态路由找不到模块的问题

--- pinda-authority-ui/src/router/index.js
+++ pinda-authority-ui/src/router/index.js
@@ -1,191 +1,197 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Layout from '@/layout'
-import db from '@/utils/localstorage'
-import store from '@/store/index'
-import loginApi from '@/api/Login.js'
-import NProgress from 'nprogress'
-import 'nprogress/nprogress.css'
-Vue.use(Router)
+import Vue from "vue";
+import Router from "vue-router";
+import Layout from "@/layout";
+import db from "@/utils/localstorage";
+import store from "@/store/index";
+import loginApi from "@/api/Login.js";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+Vue.use(Router);
 
 const constRouter = [
   {
-    path: '/redirect',
+    path: "/redirect",
     component: Layout,
-    redirect: '/redirect',
+    redirect: "/redirect",
     hidden: true,
     children: [
       {
-        path: '/redirect/:path*',
-        component: () => import('@/views/redirect/index')
+        path: "/redirect/:path*",
+        component: () => import("@/views/redirect/index")
       }
     ]
   },
   {
-    path: '/404',
-    component: () => import('@/views/error-page/404'),
+    path: "/404",
+    component: () => import("@/views/error-page/404"),
     hidden: true
   },
   {
-    path: '/login',
-    name: '登录页',
-    component: () => import('@/views/login/index')
+    path: "/login",
+    name: "登录页",
+    component: () => import("@/views/login/index")
   },
   {
-    path: '/',
+    path: "/",
     component: Layout,
-    redirect: '/dashboard',
+    redirect: "/dashboard",
     children: [
       {
-        path: 'dashboard',
-        component: () => import('@/views/dashboard/index'),
-        name: 'Dashboard',
-        meta: { title: 'dashboard', icon: 'dashboard', affix: true }
+        path: "dashboard",
+        component: () => import("@/views/dashboard/index"),
+        name: "Dashboard",
+        meta: { title: "dashboard", icon: "dashboard", affix: true }
       }
     ]
   },
   {
-    path: '/profile',
+    path: "/profile",
     component: Layout,
-    redirect: '/profile/index',
+    redirect: "/profile/index",
     hidden: true,
     children: [
       {
-        path: 'index',
-        component: () => import('@/views/profile/index'),
-        name: 'Profile',
-        meta: { title: 'profile', icon: 'user', noCache: true }
+        path: "index",
+        component: () => import("@/views/profile/index"),
+        name: "Profile",
+        meta: { title: "profile", icon: "user", noCache: true }
       },
       {
         hidden: true,
-        path: '/sms/manage/edit',
-        component: () => import('@/views/pinda/sms/manage/Edit'),
-        name: 'smsEdit',
+        path: "/sms/manage/edit",
+        component: () => import("@/views/pinda/sms/manage/Edit"),
+        name: "smsEdit",
         meta: {
-          title: '发送短信', icon: '', noCache: true
+          title: "发送短信",
+          icon: "",
+          noCache: true
         }
       }
     ]
   },
   {
-    path: '/error',
+    path: "/error",
     component: Layout,
-    redirect: 'noRedirect',
-    name: 'ErrorPages',
+    redirect: "noRedirect",
+    name: "ErrorPages",
     meta: {
-      title: 'errorPages',
-      icon: '404'
+      title: "errorPages",
+      icon: "404"
     },
     children: [
       {
-        path: '404',
-        component: () => import('@/views/error-page/404'),
-        name: 'Page404',
-        meta: { title: 'page404', noCache: true }
+        path: "404",
+        component: () => import("@/views/error-page/404"),
+        name: "Page404",
+        meta: { title: "page404", noCache: true }
       }
     ]
   },
   {
-    path: '/myiframe',
+    path: "/myiframe",
     component: Layout,
-    redirect: '/myiframe',
-    children: [{
-      path: ":routerPath",
-      name: 'iframe',
-      component: () => import('@/views/iframe/index'),
-      props: true
-    }]
-
+    redirect: "/myiframe",
+    children: [
+      {
+        path: ":routerPath",
+        name: "iframe",
+        component: () => import("@/views/iframe/index"),
+        props: true
+      }
+    ]
   }
-
-]
+];
 
 const router = new Router({
   scrollBehavior: () => ({ y: 0 }),
   routes: constRouter
-})
-const whiteList = ['/login']
+});
+const whiteList = ["/login"];
 
-let asyncRouter
+let asyncRouter;
 
 // 导航守卫，渲染动态路由
 router.beforeEach((to, from, next) => {
-  NProgress.start()
+  NProgress.start();
   if (whiteList.indexOf(to.path) !== -1) {
-    next()
+    next();
   } else {
-    const token = db.get('TOKEN')
-    const user = db.get('USER')
-    const userRouter = get('USER_ROUTER')
+    const token = db.get("TOKEN");
+    const user = db.get("USER");
+    const userRouter = get("USER_ROUTER");
     if (token.length && user) {
       if (!asyncRouter) {
         if (!userRouter) {
-          loginApi.getRouter({})
-            .then((response) => {
-              const res = response.data
-              asyncRouter = res.data
-              store.commit('account/setRoutes', asyncRouter)
-              save('USER_ROUTER', asyncRouter)
-              go(to, next)
-            })
+          loginApi.getRouter({}).then(response => {
+            const res = response.data;
+            asyncRouter = res.data;
+            store.commit("account/setRoutes", asyncRouter);
+            save("USER_ROUTER", asyncRouter);
+            go(to, next);
+          });
         } else {
-          asyncRouter = userRouter
-          go(to, next)
+          asyncRouter = userRouter;
+          go(to, next);
         }
       } else {
-        next()
+        next();
       }
     } else {
-      if (to.path === '/login') {
-        next()
+      if (to.path === "/login") {
+        next();
       } else {
-        next('/login')
+        next("/login");
       }
     }
   }
-})
+});
 
 router.afterEach(() => {
-  NProgress.done()
-})
+  NProgress.done();
+});
 
 function go(to, next) {
-  asyncRouter = filterAsyncRouter(asyncRouter)
-  router.addRoutes(asyncRouter)
-  next({ ...to, replace: true })
+  asyncRouter = filterAsyncRouter(asyncRouter);
+  router.addRoutes(asyncRouter);
+  next({ ...to, replace: true });
 }
 
 function save(name, data) {
-  localStorage.setItem(name, JSON.stringify(data))
+  localStorage.setItem(name, JSON.stringify(data));
 }
 
 function get(name) {
-  return JSON.parse(localStorage.getItem(name))
+  return JSON.parse(localStorage.getItem(name));
 }
 
 function filterAsyncRouter(routes) {
-  return routes.filter((route) => {
-    const component = route.component
+  return routes.filter(route => {
+    const component = route.component;
     if (component) {
-      if (route.component === 'Layout') {
-        route.component = Layout
+      if (route.component === "Layout") {
+        route.component = Layout;
       } else {
-        route.component = view(component)
+        console.log(component);
+        route.component = view(component);
       }
       if (route.children && route.children.length) {
-        route.children = filterAsyncRouter(route.children)
+        route.children = filterAsyncRouter(route.children);
       }
-      return true
+      return true;
     }
-  })
+  });
 }
 
 function view(path) {
-  return function (resolve) {
-    import(`@/views/${path}.vue`).then(mod => {
-      resolve(mod)
-    })
-  }
+  return function(resolve) {
+    // import(`@/views/${path}.vue`).then(mod => {
+    //   resolve(mod);
+    // });
+    require([`@/views/${path}.vue`], resolve);
+    // import(`@/views/pinda/user/user/index.vue`).then(mod => {
+    //   resolve(mod);
+    // });
+  };
 }
 
-export default router
+export default router;
